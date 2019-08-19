203b640340125c03fef5c5eab800bf6db8a2ed00
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-19 16:10:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4, 
       rent_in_dollars: {
-        ownership1: 1,
+        one_piece_owned: 25,
         ownership2: 2,
         ownership3: 3,
         ownership4: 4
