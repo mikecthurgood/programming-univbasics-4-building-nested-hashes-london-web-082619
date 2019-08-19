a1d6992612bf03e66a919ec99a409bbf8086aedc
Mike Thurgood <mikecthurgood@gmail.com>
2019-08-19 16:10:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,9 +26,9 @@ def monopoly_with_third_tier
       pieces: 4, 
       rent_in_dollars: {
         one_piece_owned: 25,
-        ownership2: 2,
-        ownership3: 3,
-        ownership4: 4
+        two_pieces_owned: 50,
+        three_pieces_owned: 75,
+        four_pieces_owned: 100
       },
       names: {
         mortlake_station: "awesome",
